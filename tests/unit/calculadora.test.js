ab3f6c87b2281c859c5d306e440786b409949c9e
Jorge Teles <jorteles@gmail.com>
2023-10-19 00:04:28 +0000

    Ajustando diretórios dos testes

--- tests/unit/calculadora.test.js
+++ tests/unit/calculadora.test.js
@@ -1,17 +1,16 @@
-const { somar } = require("../../models/calculadora.js");
-const calculadora = require("../../tests/unit/calculadora.js");
+const calculadora = require("../../models/calculadora.js");
 
 test("Somar 2 + 2", () => {
-  const resultado = somar(2, 2);
+  const resultado = calculadora.somar(2, 2);
   expect(resultado).toBe(4);
 });
 
 test("Somar 5 + 100 deveria retornar 105", () => {
-  const resultado = somar(5, 100);
+  const resultado = calculadora.somar(5, 100);
   expect(resultado).toBe(105);
 });
 
 test("Somar 'banana' + 100  deveria retornar 'Erro'", () => {
-  const resultado = somar("banana", 100);
+  const resultado = calculadora.somar("banana", 100);
   expect(resultado).toBe("Erro");
 });
